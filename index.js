bf1bd22c45ff21cd3091a43db11f2aa4d1728e82
Ertu <46158549+Er2ch@users.noreply.github.com>
2020-01-20 17:50:05 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const
     client = new Discord.Client(),
     config = require('./config.json')
 
-client.on('ready', () => {console.log(`Rainbow bot is activated\nServer ID: ${config.}`); setInterval(() => client.guilds.get(config.server).roles.find('name', config.role).edit({color: 'RANDOM'}), config.time);})
+client.on('ready', () => {console.log(`Rainbow bot is activated\nServer ID: ${config.}`); setInterval(() => client.guilds.get(config.server).roles.find('id', config.role).edit({color: 'RANDOM'}), config.time);})
 
 client.on('message', msg => {
   if (msg.content === `${config.prefix}ping`)
