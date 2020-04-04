63170248e1df0d971f770f678ca8bc6ecaf90125
ThePersigg <49908303+perssBest@users.noreply.github.com>
2020-04-04 13:33:31 +0500

    Update index.js

--- index.js
+++ index.js
@@ -3,16 +3,16 @@ const
     client = new Discord.Client(),
     config = require('./config.json')
 
-client.on('ready', () => {
-  console.log(`Rainbow bot is activated\nServer ID: ${config.server}`); 
-  setInterval(() => {
-    client.guilds.get(config.server).roles.find('id', config.role).edit({color: 'RANDOM'});
-  },config.time)
-})
+bot.on('ready', () => {
+    console.log(`Rainbow bot is activated\nServer ID: ${config.server}`); 
+    setInterval(() => {
+     bot.guilds.cache.get(config.server).roles.cache.find(u => u.id === config.role).edit({color: 'RANDOM'});
+    },config.time)
+  })
 
 client.on('message', msg => {
   if (msg.content === `${config.prefix}ping`)
-    msg.reply(`🏓 Pong!\nPing with Discord server: **${Math.floor(client.ping)}**ms`);
+    msg.reply(`🏓 Pong!\nPing with Discord server: **${Math.floor(client.ws.ping)}**ms`);
 })
 
 client.login(config.token)
